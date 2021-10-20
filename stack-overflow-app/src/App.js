ebb9e21917bf1b2d3050a928c256eb89aa2679cb
j-5844 <juhirao5844@hotmail.com>
2021-10-20 12:22:27 -0400

    Navbar and Home Page (Login/Signup) updated

--- stack-overflow-app/src/App.js
+++ stack-overflow-app/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NavigationBar, Header, HomePage, QuestionPage } from "./Components";
+import { NavigationBar, HomePage, QuestionPage } from "./Components";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavigationBar />
-        <Header />
+        
         <Switch>
           <Route path="/" exact component={() => <HomePage />} />
           <Route path="/question" exact component={() => <QuestionPage />} />
--- stack-overflow-app/src/Components/NavigationBar.js
+++ stack-overflow-app/src/Components/NavigationBar.js
@@ -1,17 +1,29 @@
 import { Link, withRouter } from 'react-router-dom';
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import {Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import './Login.css';
+
 
 const NavigationBar = () => {
     return (
         <div className="navigationBar">
-            <div class="container">
-                <Link class="navBarBrand" to="/"> Stacked Over </Link>
-            </div>
-
-            <div>
-                <Link className="navLink" to="/"> Home </Link>
-                <Link className="navLink" to="/question"> Ask Question Page </Link>
-            </div>
+            
+        <Navbar bg="light" sticky="top" >
+        <Container>
+          <Navbar.Brand style={{fontFamily:'Teko',fontSize:'30px'}}>Stacked Over</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" style={{fontSize:'15px', fontFamily:'sans-serif'}}>
+              <Nav.Link href="http://localhost:3000/">Home</Nav.Link>
+              <Nav.Link href="http://localhost:3000/question">Questions</Nav.Link>
+              <NavDropdown title="Profile">
+                <NavDropdown.Item href="http://localhost:3000/">Sign Up/Login</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        </Navbar>
 
         </div>
     )

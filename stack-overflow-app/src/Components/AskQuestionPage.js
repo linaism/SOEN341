ba7f8cd8326891f4f07ebd9786a3f75381b215da
Anusha-Reddy-Lattupally <anushareddy.lattupally@gmail.com>
2021-11-08 14:40:08 -0500

    #31 Modified AskQuestionPage

--- stack-overflow-app/src/Components/AskQuestionPage.js
+++ stack-overflow-app/src/Components/AskQuestionPage.js
@@ -4,13 +4,15 @@ import Axios from 'axios';
 
 const AskQuestionPage = () => {
 
-  const [title, setTitle] = useState(''); 
+  const [question, setQuestion] = useState(''); 
   const [content, setContent] = useState(''); 
 
   const [questionList, setQuestionList] = useState([]);
 
-  const [redirect, setRedirect] = useState(false);
+  //const [redirect, setRedirect] = useState(false);
+  const [questionSubmitted, setQuestionSubmittedFlag] = useState(false);
   const history = useHistory();
+
 
   const routeChange = () =>{ 
     let path = '/view-question'; 
@@ -30,36 +32,117 @@ const AskQuestionPage = () => {
         },
       ]);
     });
+    setQuestionSubmittedFlag(true);
   };
+  const Askquestion ={
+    fontSize: 30,
+    textAlign: "left",
+    paddingTop: "100px",
+    paddingLeft: "0px",
+    fontFamily: "Arial",
+
+  }
+  const title = {
+    fontSize: 30,
+    textAlign: "left",
+    paddingTop: "10px",
+    paddingLeft: "0px",
+    fontFamily: "Ubuntu",
+  }
+
+  const container = {
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    PaddingTop: "20px",
+    PaddingBotton:"20px",
+  }
+
+  
+  const buttonStyle1 = {
+    fontSize: 20,
+    color: "white",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    paddingRight: "10px",
+    paddingLeft: "10px",
+    marginLeft: "auto",
+    marginRight: "auto", 
+    backgroundColor: "#3E86C2",
+    borderRadius: 5,
+    borderColor: "#3E86C2",
+    width: '270px', 
+    marginBottom: 50
+  }
+
+  const inputStyle1 = {
+    fontSize: 20,
+    color: "black",
+    padding: "0px",
+    backgroundColor: "#F3EFEE",
+    borderRadius: 5,
+    borderColor: "#3E86C2",
+    fontType: "Ubuntu",
+    margin: "0 0 0 0",
+    width: '100%',
+    border:'0px',
+    outline:'0px'
+
+  }
 
   return (
-    <div >
-      <h1>Ask Question</h1>
-      <form onSubmit={ask}>
-        <h3>Title </h3>
-        <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center'}}>
-          <input 
-            type="text"
-            placeholder="Enter title"
+    <div style = {container}>
+{ !questionSubmitted && 
+<div> <h2 style={Askquestion}> <b>Ask a question</b>  </h2> 
+        <b><hr/> </b>
+        <br/>
+          <h3 style={title}> Question </h3>
+          <p> Insert a title that displays your question </p>
+          <div >
+          <form onSubmit={ask}>
+              <div style={{display: 'flex', alignSelf: 'left', textAlign: 'left', width: '70%'}}>
+              <input 
+           type="text"
+                border='none'
+                outline='none'
+                placeholder="E.g How to connect MySQL database to react app"
+                style={inputStyle1}
             onChange={(e) => {
-              setTitle(e.target.value);
+              setQuestion(e.target.value);
+              
             }} 
           />
-        </div>
-        <h3>Content</h3>
-        <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center'}}>
-          <input 
-            type="text"
-            placeholder="Enter content"
+
+              </div>
+              <h3 style={title}> Content </h3>
+              <p> Write your question here, you can go into as much detail as you want </p>              
+            <div style={{display: 'flex', alignSelf: 'left', textAlign: 'left', width: '70%'}}>
+             
+          <textarea 
+           type="text"
+           rows='5'
+           border='0px'
+           outline='0px'
+           placeholder="Explain your question..."
+           style={inputStyle1}
             onChange={(e) => {
               setContent(e.target.value);
-            }} 
-          />
-        </div>
-        <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center'}}>
-          <button>Submit Question</button>
-        </div>
-      </form>
+            }} />
+              </div>
+              
+              <div style={{ display: 'flex', alignSelf: 'right', textAlign: 'right', position: 'absolute',
+    bottom: '0px'}}>
+              <button style={buttonStyle1}>Submit Question</button>
+              </div>
+            </form>
+          </div>
+
+      </div>
+}
+{questionSubmitted && 
+<div> <div style={{alignSelf: 'center', textAlign: 'center'}}> <b>Question:</b> {question}</div>
+<div style={{alignSelf: 'center', textAlign: 'center'}}> <b>Content:</b> {content}</div>
+<h3 style={{alignSelf: 'center', textAlign: 'center', paddingTop: '50px', color: '#03AC13'}}> Thank you! Your question has been submitted successfully</h3>
+  </div>}
     </div>
   );
 

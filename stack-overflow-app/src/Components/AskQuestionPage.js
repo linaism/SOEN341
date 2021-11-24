e53266fa30abfe10df3d8555c8de67f0be57276c
Lina Ismail <ilina19@hotmail.com>
2021-11-23 21:08:08 -0500

    #34 Made modifications

--- stack-overflow-app/src/Components/AskQuestionPage.js
+++ stack-overflow-app/src/Components/AskQuestionPage.js
@@ -4,6 +4,7 @@ import {Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 const AskQuestionPage = () => {
 
+  const [questionSubmitted, setQuestionSubmittedFlag] = useState(false);
   const [question, setQuestion] = useState(''); 
   const [content, setContent] = useState(''); 
   const [userId, setUserId] = useState(0);
@@ -13,7 +14,14 @@ const AskQuestionPage = () => {
 
   Axios.defaults.withCredentials = true;
 
-  const [questionSubmitted, setQuestionSubmittedFlag] = useState(false);
+  useEffect(() => {
+    Axios.get("http://localhost:5001/login").then((response) => {
+      if (response.data.loggedIn === true) {
+        setLoginStatus(response.data.loggedIn);
+        setUserId(response.data.user[0].id);
+      }  
+    });
+  }, []);
 
   const ask = () => {
     if (loginStatus) {
@@ -31,6 +39,7 @@ const AskQuestionPage = () => {
           },
         ]);
       });
+      setQuestionSubmittedFlag(true);
     } 
   };
 
@@ -40,156 +49,96 @@ const AskQuestionPage = () => {
     }
   };
   const Askquestion ={
-    fontSize: 30,
+    fontSize: 40,
     textAlign: "left",
     paddingTop: "100px",
-    paddingLeft: "0px",
-    fontFamily: "Arial",
+    fontFamily: 'Teko',
 
   }
   const title = {
-    fontSize: 30,
+    fontSize: 24,
     textAlign: "left",
-    paddingTop: "10px",
-    paddingLeft: "0px",
-    fontFamily: "Ubuntu",
+    paddingTop: "5px",
   }
 
-  const container = {
-    paddingLeft: "20px",
-    paddingRight: "20px",
-    PaddingTop: "20px",
-    PaddingBotton:"20px",
+  const questionBox = {
+    position: 'center',
+    margin: "5px 20px 20px 20px", 
+    backgroundColor: "#F9FAFD",
+    borderRadius: "11px", 
+    padding: "20px"
   }
-
   
   const buttonStyle1 = {
-    fontSize: 20,
+    fontSize: 18,
     color: "white",
-    paddingTop: "10px",
-    paddingBottom: "10px",
-    paddingRight: "10px",
-    paddingLeft: "10px",
-    marginLeft: "auto",
-    marginRight: "auto", 
-    backgroundColor: "#3E86C2",
+    padding: "10px",
+    backgroundColor: "#8C98AF",
     borderRadius: 5,
-    borderColor: "#3E86C2",
-    width: '270px', 
-    marginBottom: 50
+    borderColor: "#8C98AF",
+    width: '180px', 
+    margin: "10px 0px 10px 0px"
   }
 
   const inputStyle1 = {
-    fontSize: 20,
-    color: "black",
-    padding: "0px",
-    backgroundColor: "#F3EFEE",
-    borderRadius: 5,
-    borderColor: "#3E86C2",
-    fontType: "Ubuntu",
-    margin: "0 0 0 0",
+    padding: "10px",
+    backgroundColor: "white",
     width: '100%',
     border:'0px',
-    outline:'0px'
-
   }
 
-  useEffect(() => {
-    Axios.get("http://localhost:5001/login").then((response) => {
-      if (response.data.loggedIn === true) {
-        setLoginStatus(response.data.loggedIn);
-        setUserId(response.data.user[0].id);
-      }  
-    });
-  }, []);
-
   return (
-<<<<<<< HEAD
-    <div style = {container}>
-{ !questionSubmitted && 
-<div> <h2 style={Askquestion}> <b>Ask a question</b>  </h2> 
-        <b><hr/> </b>
-        <br/>
-          <h3 style={title}> Question </h3>
-          <p> Insert a title that displays your question </p>
-          <div >
-          <form onSubmit={ask}>
-              <div style={{display: 'flex', alignSelf: 'left', textAlign: 'left', width: '70%'}}>
-              <input 
-           type="text"
-                border='none'
-                outline='none'
-                placeholder="E.g How to connect MySQL database to react app"
-                style={inputStyle1}
-=======
-    <div >
-      <h1 style={{fontFamily:'Teko',fontSize:'30px', paddingTop:'40px', paddingLeft:'40px'}}>Ask Question</h1>
-      <form onSubmit={ask}>
-        <h3 style={{fontSize:'15px', fontFamily:'sans-serif', paddingTop:'10px', paddingLeft:'60px'}}>Title </h3>
-        <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center', paddingLeft:'60px'}}>
-          <input style={{paddingLeft:'10px'}}
-            type="text"
-            placeholder="Enter title"
->>>>>>> 344f888f1b2651c0c42ce261f184fe4fd208f5b8
-            onChange={(e) => {
-              setQuestion(e.target.value);
-              
-            }} 
-          />
-<<<<<<< HEAD
-
+  <div className="container"> 
+    {!questionSubmitted && 
+      <div> 
+        <div>
+          <h2 style={Askquestion}> Ask a question </h2> 
+        </div>
+        <div style={questionBox}>
+          <h3 style={title}> Title </h3>
+          <p> Insert a title that describes your question </p>
+          <div>
+            <form onSubmit={ask}>
+              <div>
+                <input style={inputStyle1} 
+                  type="text"
+                  border='none'
+                  outline='none'
+                  placeholder="E.g How to connect MySQL database to react app"
+                  onChange={(e) => {setQuestion(e.target.value);}}
+                />
               </div>
-              <h3 style={title}> Content </h3>
-              <p> Write your question here, you can go into as much detail as you want </p>              
-            <div style={{display: 'flex', alignSelf: 'left', textAlign: 'left', width: '70%'}}>
-             
-          <textarea 
-           type="text"
-           rows='5'
-           border='0px'
-           outline='0px'
-           placeholder="Explain your question..."
-           style={inputStyle1}
-            onChange={(e) => {
-              setContent(e.target.value);
-            }} />
+              <h3 style={title}> Question </h3>
+              <p> Write your question here. You can go into as much detail as you want </p>              
+              <div>
+                <textarea 
+                  type="text"
+                  rows='5'
+                  border='0px'
+                  outline='0px'
+                  placeholder="Explain your question..."
+                  style={inputStyle1} 
+                  onChange={(e) => {setContent(e.target.value);}}
+                />
               </div>
-              
-              <div style={{ display: 'flex', alignSelf: 'right', textAlign: 'right', position: 'absolute',
-    bottom: '0px'}}>
-           <button style={buttonStyle1}>Submit Question</button>
+              <div style={{ alignSelf: 'left', textAlign: 'right', bottom: '0px'}}>
+                <button style={buttonStyle1} onClick={submit}>Submit Question</button>
               </div>
             </form>
           </div>
-
+        </div>
       </div>
-}
-{questionSubmitted && 
-<div> <div style={{alignSelf: 'center', textAlign: 'center'}}> <b>Question:</b> {question}</div>
-<div style={{alignSelf: 'center', textAlign: 'center'}}> <b>Content:</b> {content}</div>
-<h3 style={{alignSelf: 'center', textAlign: 'center', paddingTop: '50px', color: '#03AC13'}}> Thank you! Your question has been submitted successfully</h3>
-<div style={{alignSelf: 'center', textAlign: 'center', paddingTop: '10px'}}> <Nav.Link href="http://localhost:3000/answer">To navigate to answer screen click here</Nav.Link> </div>
-          </div>}
-=======
-        </div>
-        <h3 style={{fontSize:'15px', fontFamily:'sans-serif', paddingTop:'10px', paddingLeft:'60px'}}>Question</h3>
-        <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center', paddingLeft:'60px', paddingBottom:'20px'}}>
-          <input style={{paddingLeft:'10px', paddingBottom:'50px', paddingRight:'200px'}}
-            type="text"
-            placeholder="Enter content"
-            onChange={(e) => {
-              setContent(e.target.value);
-            }} 
-          />
-        </div>
-        <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center',  paddingLeft:'60px'}}>
-        <button style={{fontSize:'15px', fontFamily:'sans-serif'}} onClick={submit}>Submit Question</button>
-        </div>
-      </form>
->>>>>>> 344f888f1b2651c0c42ce261f184fe4fd208f5b8
-    </div>
+    }
+    {questionSubmitted && 
+    <div style={{margin: "15%"}}> 
+      <h3 style={title, {marginTop: "15%"}}>Title</h3>
+      <p style={{fontSize: 20}}> {question}</p>
+      <h3 style={title}>Question</h3>
+      <p style={{fontSize: 20}}> {content}</p>
+      <h3 style={{alignSelf: 'center', textAlign: 'center', paddingTop: '50px', color: '#03AC13'}}> Thank you! Your question has been submitted successfully</h3>
+    </div>}
+  </div>
   );
-
 }
 
 export default AskQuestionPage

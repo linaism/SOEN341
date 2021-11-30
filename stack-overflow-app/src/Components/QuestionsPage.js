8e7f223746a7bfb4ef8a0287f51e4cdcde15dcb7
j-5844 <juhirao5844@hotmail.com>
2021-11-30 14:12:28 -0500

    Search by keyword
    Searching by keyword has been implemented

--- stack-overflow-app/src/Components/QuestionsPage.js
+++ stack-overflow-app/src/Components/QuestionsPage.js
@@ -20,25 +20,46 @@ const QuestionsPage = () => {
     });
   };
 
+  const[searchTitle, setSearchTitle] = useState('')
+
   return (
     <div>
-      {questionList.map((question) => {
+      {/*<div style={{padding:'50px', paddingBottom:'0px'}}>
+        <input type="text" placeholder="Search..." onChange={event => {setSearchTitle(event.target.value)}}></input>
+      </div>
+      {questionList.filter((question) => {
+        if (searchTitle == ""){
+          return question
+        } else if (question.title.toLowerCase().includes(searchTitle.toLowerCase())){
+          return question
+        } 
+      }).map((question) => {*/}
+      <div style={{padding:'50px', paddingBottom:'0px'}}>
+        <input type="text" placeholder="Search..." onChange={event => {setSearchTitle(event.target.value)}}></input>
+      </div>
+      {questionList.filter((question) => {
+        if (searchTitle == ""){
+          return question
+        } else if (question.title.toLowerCase().includes(searchTitle.toLowerCase())){
+          return question
+        } 
+      }).map((question) => {
           return (
             <div key={question.question_id}>
               <h1 style={{fontFamily:'Teko',fontSize:'30px', paddingTop:'40px', paddingLeft:'40px'}}>Question</h1>
               <div >
-              <p style={{fontSize:'15px', fontFamily:'sans-serif', paddingLeft:'60px'}}>Title: {question.title}</p>
-              <Link  style={{textDecoration:'none'}} to={{
-                pathname: `/view-question/${question.question_id}`,
-                state: { question: question }
-              }}>
+                <p style={{fontSize:'15px', fontFamily:'sans-serif', paddingLeft:'60px'}}>Title: {question.title}</p>
+                <Link  style={{textDecoration:'none'}} to={{
+                  pathname: `/view-question/${question.question_id}`,
+                  state: { question: question }
+                }}>
                 <div style={{display: 'flex', alignSelf: 'center', textAlign: 'center',  paddingLeft:'60px'}}>
                   <Button>
                     <h3 style={{fontSize:'15px', fontFamily:'sans-serif', paddingTop:'5px'}}>View Question</h3>
                   </Button>
                 </div>
-              </Link>
-            </div>
+                </Link>
+              </div>
             </div>
           );
         })}
--- stack-overflow-app/src/server/server.js
+++ stack-overflow-app/src/server/server.js
@@ -66,6 +66,28 @@ app.get("/questions-get", (req, res) => {
     });
   });
 
+// Get all tags
+app.get("/tags-get", (req, res) => {
+  questionsDB.query("SELECT * FROM tags_info", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+  // Get a tag by id to view
+  app.get("/view/:tagid", (req, res) => {
+    questionsDB.query("SELECT * FROM tags_info WHERE id = ?", id, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
   // Get a question by id to view
   app.get("/view/:id", (req, res) => {
     questionsDB.query("SELECT * FROM question_info WHERE id = ?", id, (err, result) => {
